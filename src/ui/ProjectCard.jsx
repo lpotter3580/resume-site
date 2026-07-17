eb5f0a8ab55ff17a029825112167e28805a259fd
Logan Potter <lpotter3580@gmail.com>
2026-07-17 19:26:45 -0400

    some media queries not all yet

--- src/ui/ProjectCard.jsx
+++ src/ui/ProjectCard.jsx
@@ -1,18 +1,22 @@
 export default function ProjectCard({title, description, startDate, endDate, role, skillsList, imgUrl, link}){
     return (
         <>
-        <section class="bubble container">
-            <img src={imgUrl} alt="Alt text placeholder" class="project-size"></img>
-            <div>
+        <section class="bubble">
+            <div class="container">
+                <img src={imgUrl} alt="Alt text placeholder" class="project-size"></img>
                 <div>
-                    <a href={link} target="_blank"><h3>{title}</h3></a>
-                    <h4 class="right">{skillsList}</h4>
-                    <h4>{role}</h4>
-                    <h4>{startDate} - {endDate}</h4>
-                    
+                    <div>
+                        <a href={link} target="_blank"><h3>{title}</h3></a>
+                        <h4>{role}</h4>
+                        <h4>{startDate} - {endDate}</h4>
+                        <h4 class="right">{skillsList}</h4>
+                        
+                    </div>
+                    <p class="large">{description}</p>
                 </div>
-                <p>{description}</p>
+                
             </div>
+            <p class="small">{description}</p>
         </section>
         </>
     );
