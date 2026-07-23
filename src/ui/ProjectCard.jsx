df2fc3f3fa7d49c420a1b596ffc9ba0ec8d72ada
Logan Potter <lpotter3580@gmail.com>
2026-07-22 21:00:12 -0400

    mobile down to 600px, hamburger menu comes next

--- src/ui/ProjectCard.jsx
+++ src/ui/ProjectCard.jsx
@@ -2,18 +2,22 @@ export default function ProjectCard({title, description, startDate, endDate, rol
     return (
         <>
         <section class="bubble">
-            <div class="container">
+            <div class="container mb-15">
                 <img src={imgUrl} alt="Alt text placeholder" class="project-size"></img>
                 <div>
                     <div>
                         <a href={link} target="_blank"><h3>{title}</h3></a>
-                        <h4>{role}</h4>
-                        <h4>{startDate} - {endDate}</h4>
-                        <h4 class="right">{skillsList}</h4>
+                        <ul class="list">
+                            <li><h4>{role}</h4></li>
+                            <li><h4>{skillsList}</h4></li>
+                            <li><h4>{startDate} - {endDate}</h4></li>
+                        </ul>
                         
                     </div>
+                    
                     <p class="large">{description}</p>
                 </div>
+
                 
             </div>
             <p class="small">{description}</p>
