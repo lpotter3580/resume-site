4940604996d7c76c1069d67b2f72fd0a440aa7b7
Logan Potter <lpotter3580@gmail.com>
2026-07-23 17:55:26 -0400

    hamburger menu!

--- src/ui/Nav.jsx
+++ src/ui/Nav.jsx
@@ -3,6 +3,7 @@ import '../App.css';
 import LinkButton from "./LinkButton";
 import NavLinksData from "../data/links.json";
 import getNavLinks from "../data/getNavLinks";
+import { FaBars } from "react-icons/fa";
 
 export default function NavBar ({activeLink}) {
 
@@ -13,12 +14,25 @@ export default function NavBar ({activeLink}) {
         <>
         <header class="container">
             <a href="#"><img alt="Logan Potter" src={imgUrl} id="icon" onclick="advanceImage()"></img></a>
-            <nav>
+            <nav class="desktop">
                 <ul class="container">
                     {navLinks}
                 </ul>
             </nav>
+            <FaBars size={114} class="hamburger-icon mobile" onClick={loadHamburger}/>
+            <ul id="hamburgerMenu">
+                {navLinks}
+            </ul>
         </header>
         </>
     )
+}
+
+function loadHamburger(){
+    var menu = document.getElementById("hamburgerMenu");
+    if (menu.style.display === "none") {
+        menu.style.display = "flex";
+    } else {
+        menu.style.display = "none";
+    }
 }
