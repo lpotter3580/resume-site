81733d6d52094061ff3168d8bfa8bc13aadf4e7d
Logan Potter <lpotter3580@gmail.com>
2026-08-02 17:27:33 -0400

    various styling changes and alt text

--- src/ui/ProjectCard.jsx
+++ src/ui/ProjectCard.jsx
@@ -1,9 +1,9 @@
-export default function ProjectCard({title, description, startDate, endDate, role, skillsList, imgUrl, link}){
+export default function ProjectCard({title, description, startDate, endDate, role, skillsList, imgUrl, altText, link}){
     return (
         <>
         <section class="bubble">
             <div class="container mb-15">
-                <img src={imgUrl} alt="Alt text placeholder" class="project-size"></img>
+                <img src={imgUrl} alt={altText} class="project-size"></img>
                 <div>
                     <div>
                         <a href={link} target="_blank"><h3>{title}</h3></a>
