eeeaee0f958d757cd76dd622227d5db6065bc437
Logan Potter <lpotter3580@gmail.com>
2026-09-23 01:24:00 -0400

    list of hobbies

--- src/Pages/OtherProjects.jsx
+++ src/Pages/OtherProjects.jsx
@@ -1,23 +1,31 @@
-function generateProjects(){
+import HobbyCard from "../ui/HobbyCard";
+import HobbyData from "../data/hobbies"
+
+function generateHobbies(){
     const HobbyDataArr = [];
-    Object.keys(ProjectData).forEach(key => ProjectDataArr.push({title: ProjectData[key].title, description: ProjectData[key].description, startDate:ProjectData[key].startDate, endDate: ProjectData[key].endDate, role: ProjectData[key].role, skillsList: ProjectData[key].skillsList, imgUrl: ProjectData[key].imgUrl, altText: ProjectData[key].altText, link: ProjectData[key].link}));
+    Object.keys(HobbyData).forEach(key => HobbyDataArr.push({title: HobbyData[key].title, description: HobbyData[key].description, startDate:HobbyData[key].startDate, endDate: HobbyData[key].endDate, role: HobbyData[key].role, skillsList: HobbyData[key].skillsList, imgUrl: HobbyData[key].imgUrl, altText: HobbyData[key].altText, link: HobbyData[key].link}));
     
-    const Projects = ProjectDataArr.map(card =>
-        <li key={card.title}><ProjectCard title={card.title} description={card.description} startDate={card.startDate} endDate={card.endDate} role={card.role} skillsList={"Skills: " + card.skillsList} imgUrl={card.imgUrl} altText={card.altText} link={card.link}/></li>
+    const Hobbies = HobbyDataArr.map(card =>
+        <li key={card.title}><HobbyCard title={card.title} description={card.description} startDate={card.startDate} endDate={card.endDate} role={card.role} imgUrl={card.imgUrl} altText={card.altText}/></li>
     );
 
     return Hobbies
 }
 
 export default function HobbiesPage(){
+    let hobbies = generateHobbies();
     return(
         <>
             <main>
                 <h1 class="whiteText">Hobbies</h1>
 
-                <p>
-                    Here you will find a list of my non-computing hobbies, clubs, and overall just what I like to do when I'm not working!
+                <p class="bubble">
+                    Here you will find a list of my non-computing hobbies, clubs, and overall what I like to do when I'm not working!
                 </p>
+
+                <ul>
+                    {hobbies}
+                </ul>
             </main>
         </>
     );
